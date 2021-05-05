6c3b6e29fbdc47cf98f6e64021bcd9f0495e666c
William da Silva <CIANDT\williamdasilva@lnb028324cps.ciandt.global>
2021-05-05 08:14:31 -0300

    ajuste no mock

--- mock/mock.sh
+++ mock/mock.sh
@@ -6,6 +6,6 @@ curl --request GET \
      --url 'https://repo1.maven.org/maven2/com/github/tomakehurst/wiremock-standalone/2.27.2/wiremock-standalone-2.27.2.jar'\
      --output 'app.jar'
 
-java -jar app.jar --port 8889
+java -jar app.jar --port 8889 --local-response-templating
 
 rm -rf app.jar
